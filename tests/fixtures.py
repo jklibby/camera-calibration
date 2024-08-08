c426dba2200ea98a2e1fe25db4da43b79faf12d9
sdshmkh <36247925+sdshmkh@users.noreply.github.com>
2024-08-08 14:09:22 -0400

    GHA (#15)
    * GHA
    
    * fix requirements.txt
    
    * remove pywin
    
    * download calibration file action added
    
    * check for cc download action
    
    * added caching
    
    * print pwd
    
    * fix typo
    
    * corrected syntax
    
    * fixed input syntax
    
    * passed file_id as a secret
    
    * use with to pass secret
    
    * use inputs again
    
    * log
    
    * use sectrets instead
    
    * use env instead
    
    * check for secest
    
    * pass sectre to env
    
    * added env
    
    * fix typo
    
    * remove logs and comments
    
    * fix test fixtures

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,4 +1,5 @@
 import pytest
+from pathlib import Path
 import yaml
 
 from calibrator import CalibratorConfig
@@ -12,8 +13,8 @@ config_dict: CalibratorConfig = {
         'left_cam_id': 0,
         'right_cam_id': 1, 
         'image_count': 50, 
-        'image_path': '../calibration-images/calibration-images/single_images/', # need to change
-        'stereo_images_path': '../calibration-images/calibration-images/stereo_images/', 
+        'image_path': 'test_images/calibration-images/calibration-images/single_images/',
+        'stereo_images_path': 'test_images/calibration-images/calibration-images/stereo_images/', 
         'stereo_images_count': 50
     },
     'camera_calibration': {
@@ -35,8 +36,9 @@ config_dict: CalibratorConfig = {
 def calibration_yaml_fixture(tmp_path_factory):
     # dump config yaml into a tmp file
     yaml_data = yaml.dump(config_dict)
-    yaml_file = tmp_path_factory.mktemp("test_data") / "test_calibrator_config.yaml"
+    yaml_file:Path = tmp_path_factory.mktemp("config") / "test_calibrator_config.yaml"
     with open(yaml_file, "+w") as f:
         f.write(yaml_data)
         f.close()
-    return yaml_file
+    yield yaml_file
+    
